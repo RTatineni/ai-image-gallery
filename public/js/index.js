939f59f016dd62dd0b70e2a5550e56a5db528e50
Aaron Nealy <50381328+aaronnealy@users.noreply.github.com>
2019-07-18 09:57:09 -0500

    modal working

--- public/js/index.js
+++ public/js/index.js
@@ -1,99 +1,49 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+// function openModal() {
+//   $("#myModal").show() = "block";
+// }
 
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  }
-};
+// function closeModal() {
+//   $("#myModal").css("display", "none");
+// }
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-      $li.append($button);
+// function showSlides(n) {
+//   var i;
+//   var slides = $(".mySlides");
+//   var dots = $(".demo");
+//   var captionText = $("#caption");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].css("display", "none");
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].attr("class") = dots[i].attr("class").replace(" active", "");
+//   }
+//   slides[slideIndex-1].show() = "block";
+//   dots[slideIndex-1].attr("class") += " active";
+//   captionText.html = dots[slideIndex-1].alt;
+// }
 
-      return $li;
-    });
+// $('#myCarousel').on('slide.bs.carousel', function () {
+//   // do something...
+// })
 
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
+$(function(){
 
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
-
-  API.saveExample(example).then(function() {
-    refreshExamples();
+  $('.carousel-control').click(function(e){
+      e.preventDefault();
+      $("#aaron-modal").carousel( $(this).data() );
   });
 
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+});
