4013ef5d8440ab6b775fe48629d41cc52ff12c71
hausu <kennedymj2@gmail.com>
2019-07-18 10:07:32 -0500

    mysql db, apiRoutes, htmlpage

--- models/example.js
+++ models/example.js
@@ -1,7 +1,56 @@
+// STARTER CODE
+// ==============================================================
+
+// module.exports = function(sequelize, DataTypes) {
+//   var Example = sequelize.define("Example", {
+//     text: DataTypes.STRING,
+//     description: DataTypes.TEXT
+//   });
+//   return Example;
+// };
+
+// INITIAL CODE / DATABASE CONFIG
+// ==============================================================
+
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var Post = sequelize.define("Post", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+      len: [1]
+      }
+    },
+    link: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1]
+    },
+    rank: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: '0'
+    },
+
+    // sequelize adds created_at by default
+
+    // created_at: {
+    //   type: 'TIMESTAMP',
+    //   defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+    //   allowNull: false 
+    // }
+    
   });
-  return Example;
+
+  Post.associate = function(models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    Post.belongsTo(models.Author, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Post;
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,68 @@
 var db = require("../models");
 
 module.exports = function(app) {
+
+  // STARTER CODE
+  // ==============================================================
+
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+  // app.get("/api/examples", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.json(dbExamples);
+  //   });
+  // });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // app.post("/api/examples", function(req, res) {
+  //   db.Example.create(req.body).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
+
+  // Delete an example by id
+//   app.delete("/api/examples/:id", function(req, res) {
+//     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
+
+  // ROUTES
+  // ==============================================================
+
+  // Get all image rankings
+  app.get("/api/ranking", function(req, res) {
+    db.Post.findAll({}).then(function(dbRanking) {
+      res.json(dbRanking);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // Create a new image ranking
+  app.post("/api/ranking", function(req, res) {
+    db.Post.create(req.body).then(function(dbRanking) {
+      res.json(dbRanking);
     });
   });
+
+  // Delete an image ranking by id
+  app.delete("/api/ranking/:id", function(req, res) {
+    db.Post.destroy({ where: { id: req.params.id } }).then(function(dbRanking) {
+      res.json(dbRanking);
+    });
+  });
+
+  // Increment image ranking
+  app.post("/api/ranking", function(req, res){
+    db.Post.increment('rank', {by: 1}).then(function(dbRanking){
+      res.json(dbRanking);
+    });
+  });
+
+  // Decrement image ranking
+  app.post("/api/ranking", function(req, res){
+    db.Post.decrement('rank', {by: 1} ).then(function(dbRanking){
+      res.json(dbRanking);
+    });
+  });
+
+
 };
