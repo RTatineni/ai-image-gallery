08e0f15b6db16b499693ddd285033b5931523d40
Rajiv <rtatineni2@gmail.com>
2019-07-19 10:00:24 -0500

    AI model is working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,5 @@
 var db = require("../models");
 const aws = require('aws-sdk')
-
 var s3 = new aws.S3()
 
 module.exports = function(app) {
@@ -18,14 +17,15 @@ module.exports = function(app) {
       if(err)throw err;
       console.log(data);
   
-      for(let i = 0; i <3; i++){
+      for(i in data.Contents){
         var s3URL = "https://gallery-images-aa.s3.amazonaws.com/" + data.Contents[i].Key
    
       imgUrls.push(s3URL)
   
       }
       console.log(imgUrls)
-      res.render("index",{imgUrls: imgUrls[0]});
+      
+      res.render("index",{imgUrls: imgUrls[0], output:"hi"});
 
     })
   })
