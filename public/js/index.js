162224b64b81ce73de494395e42322097c786095
Rajiv <rtatineni2@gmail.com>
2019-07-19 11:38:03 -0500

    cleaned up code

--- public/js/index.js
+++
@@ -1,9 +0,0 @@
-// Get references to page elements
-$(function(){
-
-  $('.carousel-control').click(function(e){
-      e.preventDefault();
-      $("#aaron-modal").carousel( $(this).data() );
-  });
-
-});
